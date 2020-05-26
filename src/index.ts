fa94d54b200bcee26e6de8d63ff162531f581dd9
Alessandro Lioi <lioialessandro2000@live.com>
2020-05-26 10:59:56 +0200

    Added basic telegram bot functions

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,28 @@
+import Telegraf from "telegraf";
 import express from "express";
 
-const PORT = process.env.PORT || 8080;
+const { NODE_ENV } = process.env;
 
+if (NODE_ENV !== "production") require("dotenv").config();
+
+const { PORT, TELEGRAM_TOKEN, URL } = process.env;
+
+if (!TELEGRAM_TOKEN || (NODE_ENV === "production" && !URL))
+  throw new Error("Environment variables can't be null");
+
+const Bot = new Telegraf(TELEGRAM_TOKEN);
 const App = express();
+
+Bot.start((ctx) => ctx.reply("MangaComicsPortalBot"));
+
+if (NODE_ENV !== "production") {
+  console.info("Development environment");
+  Bot.startPolling();
+} else {
+  console.info("Production environment");
+  Bot.telegram.setWebhook(`${URL}/bot${TELEGRAM_TOKEN}`);
+  App.use(Bot.webhookCallback(`/bot${TELEGRAM_TOKEN}`));
+}
 
 App.get("/", (_, res) =>
   res.send(
@@ -10,4 +30,6 @@ App.get("/", (_, res) =>
   )
 );
 
-App.listen(PORT, () => console.info(`Server running on port: ${PORT}`));
+App.listen(PORT || 8080, () =>
+  console.info(`Server running on port: ${PORT || 8080}`)
+);
